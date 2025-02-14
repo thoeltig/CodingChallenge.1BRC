a8993ae4264db8d9e07f05d7dec939078dd51183
thoeltig <41591511+thoeltig@users.noreply.github.com>
2025-02-14 23:23:01 +0100

    - modified generation logic to work with fileStream - changed outputs to byte array to copy & write each line

--- src/1BRC.ConsoleRunner/MeasurementsGenerator.cs
+++ src/1BRC.ConsoleRunner/MeasurementsGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Text;
 
@@ -10,25 +11,31 @@ namespace _1BRC.ConsoleRunner {
 		public static void CreateFile(string filePath, int rowCount) {
 			var names = CreateRandomNames();
 
-			using (var writer = new StreamWriter(filePath, false, Encoding.UTF8)) {
-				writer.NewLine = "\n";
+			using (var stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write)) {
+				stream.SetLength(0);
 
 				for (var i = 0; i < rowCount; i++) {
 					var line = GetLine(names);
-					writer.WriteLine(line);
+					stream.Write(line, 0, line.Length);
 				}
 			}
 		}
 
-		private static string GetLine(string[] names) {
-			const string separator = ";";
+		private static byte[] GetLine(byte[][] names) {
+			const byte separator = 59; // ;
+			const byte newLine = 10; // \n
 
 			var name = GetRandomName(names);
 			var temperature = GetRandomTemperature();
-			return string.Concat(name, separator, temperature);
+			var result = new byte[name.Length + temperature.Length + 2];
+			Array.Copy(name, 0, result, 0, name.Length);
+			result[name.Length] = separator;
+			Array.Copy(temperature, 0, result, name.Length+1, temperature.Length);
+			result[result.Length-1] = newLine;
+			return result;
 		}
 
-		private static double GetRandomTemperature() {
+		private static byte[] GetRandomTemperature() {
 			// The random value range needs to be 1 to 1999 because the result will be divided by 10 and afterwards 100 is subtracted
 			// which will result in the new value range from -99.9 to 99.9
 			const int randomMinIncluded = 1;
@@ -37,17 +44,17 @@ namespace _1BRC.ConsoleRunner {
 			const double subtractedValue = 100;
 			const int digits = 1;
 
-			return Math.Round(_random.Next(randomMinIncluded, randomMaxExcluded) / divider - subtractedValue, digits);
+			return Encoding.UTF8.GetBytes(Math.Round(_random.Next(randomMinIncluded, randomMaxExcluded) / divider - subtractedValue, digits).ToString(CultureInfo.InvariantCulture));
 		}
 
-		private static string GetRandomName(string[] names) => names[_random.Next(MaxNameCount)];
+		private static byte[] GetRandomName(byte[][] names) => names[_random.Next(MaxNameCount)];
 
-		private static string[] CreateRandomNames() {
+		private static byte[][] CreateRandomNames() {
 			var validNameBytes = GetValidBytesForName();
-			var names = new string[MaxNameCount];
+			var names = new byte[MaxNameCount][];
 			for (var i = 0; i < MaxNameCount; i++) {
 				var nameBytes = GetRandomNameBytes(validNameBytes);
-				names[i] = Encoding.UTF8.GetString(nameBytes);
+				names[i] = nameBytes;
 			}
 
 			return names;
