89d83fd06839c15cb089eeced78229b5f8b7dde0
thoeltig <41591511+thoeltig@users.noreply.github.com>
2025-02-18 01:52:35 +0100

    - replaced log line output result table generator -> code is ugly but can copy that directly into the readMe

--- src/1BRC.Net5.ConsoleRunner/FileReadAndWrite.cs
+++ src/1BRC.Net5.ConsoleRunner/FileReadAndWrite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 
@@ -33,45 +34,49 @@ namespace _1BRC.Net5.ConsoleRunner {
 			FileOptions.SequentialScan | FileOptions.WriteThrough | FileFlagNoBuffering,
 		};
 
+		private static readonly int _optionsCount = _options.Length;
+		private static readonly TimeSpan[][][] _outputWrite = new TimeSpan[_optionsCount][][];
+		private static readonly TimeSpan[][][] _outputRead = new TimeSpan[_optionsCount][][];
+
 		public static void ExecuteTest(Action<string> progressCallback) {
-			using (var logWriter = new StreamWriter(ResultFile)) {
-				var fileContentBytes = new byte[FileSizeToWrite];
-				var random = new Random();
-				random.NextBytes(fileContentBytes);
+			var fileContentBytes = new byte[FileSizeToWrite];
+			var random = new Random();
+			random.NextBytes(fileContentBytes);
+			var chunksCount = _chunkSizes.Length;
 
-				logWriter.WriteLine($"Total content size: {FileSizeToWrite}");
+			for (var i = 0; i < _optionsCount; i++) {
+				var option = _options[i];
+				_outputWrite[i] = new TimeSpan[chunksCount][];
+				_outputRead[i] = new TimeSpan[chunksCount][];
+				progressCallback($"Start file option {option}");
 
-				foreach (var option in _options) {
-					WriteMultipleEmptyLines(logWriter);
-					logWriter.WriteLine($"File option {option}");
-					progressCallback($"Start file option {option}");
+				for (var j = 0; j < chunksCount; j++) {
+					var chunkSize = _chunkSizes[j];
+					_outputWrite[i][j] = new TimeSpan[chunksCount];
+					_outputRead[i][j] = new TimeSpan[chunksCount];
+					var splitCount = (int)Math.Ceiling(FileSizeToWrite / (double)chunkSize);
 
-					foreach (var chunkSize in _chunkSizes) {
-						WriteMultipleEmptyLines(logWriter);
-
-						var splitCount = (int)Math.Ceiling(FileSizeToWrite / (double)chunkSize);
-						logWriter.WriteLine($"Requires {splitCount} read & write operations which will each transfer {chunkSize} bytes");
-
-						WriteMultipleEmptyLines(logWriter);
-
-						foreach (var bufferSize in _chunkSizes) {
-							WriteMultipleEmptyLines(logWriter);
-							logWriter.WriteLine($"Set buffer size to {bufferSize}");
-							WriteMultipleEmptyLines(logWriter);
-
-							RunTest("FileStream.Write", () => Write(bufferSize, option, splitCount, chunkSize, fileContentBytes.AsSpan()), logWriter, DeleteFile);
-							RunTest("FileStream.Read", () => Read(bufferSize, option, chunkSize), logWriter);
-						}
+					for (var k = 0; k < chunksCount; k++) {
+						var bufferSize = _chunkSizes[k];
+						_outputWrite[i][j][k] = RunTest(() => Write(bufferSize, option, splitCount, chunkSize, fileContentBytes.AsSpan()), DeleteFile);
+						_outputRead[i][j][k] = RunTest(() => Read(bufferSize, option, chunkSize));
 					}
 				}
 			}
 
 			DeleteFile();
-			return;
 
-			void WriteMultipleEmptyLines(StreamWriter logWriter) {
-				logWriter.WriteLine();
-				logWriter.WriteLine();
+			using (var writer = new StreamWriter(ResultFile)) {
+				foreach (var line in GenerateResultTable("Write", _outputWrite)) {
+					writer.WriteLine(line);
+				}
+
+				writer.WriteLine(Environment.NewLine);
+				writer.WriteLine(Environment.NewLine);
+
+				foreach (var line in GenerateResultTable("Read", _outputRead)) {
+					writer.WriteLine(line);
+				}
 			}
 		}
 
@@ -95,17 +100,15 @@ namespace _1BRC.Net5.ConsoleRunner {
 				var buffer = new Span<byte>(new byte[chunkSize]);
 				var readBytes = 0;
 				while ((readBytes = stream.Read(buffer)) != 0) {
-                }
+				}
 			}
 		}
 
 		private static void DeleteFile() => File.Delete(TestFile);
 
-		private static void RunTest(string testName, Action testAction, StreamWriter resultWriter, Action cleanupAction = null) {
+		private static TimeSpan RunTest(Action testAction, Action cleanupAction = null) {
 			var sw = new Stopwatch();
 			var times = new TimeSpan[TestRunCount];
-
-			resultWriter.WriteLine($"Test case: {testName}");
 
 			try {
 				for (var i = 0; i < TestRunCount; i++) {
@@ -118,19 +121,53 @@ namespace _1BRC.Net5.ConsoleRunner {
 					times[i] = sw.Elapsed;
 				}
 
-				var avg = new TimeSpan(times.Sum(x => x.Ticks) / TestRunCount);
-				resultWriter.WriteLine($"Average: {avg:ss':'fffffff}");
-
+				//var avg = new TimeSpan(times.Sum(x => x.Ticks) / TestRunCount);
 				var quartile = (int)Math.Round(TestRunCount * 0.25);
 				var half = (int)Math.Round(TestRunCount * 0.5);
 				var median = new TimeSpan(times.OrderBy(x => x).Skip(quartile).Take(half).Sum(x => x.Ticks) / half);
-				resultWriter.WriteLine($"Median: {median:ss':'fffffff}");
+				return median;
 			}
 			catch {
-				resultWriter.WriteLine("Test failed...");
+				return TimeSpan.Zero;
+			}
+		}
+
+		private static string[] GenerateResultTable(string tableName, TimeSpan[][][] values) {
+			const int headerSize = 2;
+			double? firstTime = null;
+			var lines = new string[_chunkSizes.Length * _chunkSizes.Length + headerSize];
+			lines[0] = $"|{tableName}|";
+			lines[1] = "|----|";
+			for (var i = 0; i < values.Length; i++) {
+				var allValuesForOption = values[i];
+				var option = _options[i];
+				lines[0] = string.Concat(lines[0], option, "|%|");
+				lines[1] = string.Concat(lines[1], "----|----|");
+
+				for (var j = 0; j < allValuesForOption.Length; j++) {
+					var allBuffersPerChunkSize = allValuesForOption[j];
+					var bufferSize = _chunkSizes[j];
+
+					for (var k = 0; k < allBuffersPerChunkSize.Length; k++) {
+						var timeSpanPerChunk = allBuffersPerChunkSize[k];
+						var chunkSize = _chunkSizes[k];
+						var splitCount = (int)Math.Ceiling(FileSizeToWrite / (double)chunkSize);
+						var lineIdx = allBuffersPerChunkSize.Length * j + k + headerSize;
+
+						if (i == 0) {
+							lines[lineIdx] = string.Concat($"|Buffer {bufferSize} Chunk {chunkSize} ({splitCount} times)|");
+
+							if (firstTime.HasValue == false) {
+								firstTime = timeSpanPerChunk.TotalSeconds;
+							}
+						}
+
+						lines[lineIdx] = string.Concat(lines[lineIdx], timeSpanPerChunk.ToString("ss':'fffffff"), "|", Math.Round((timeSpanPerChunk.TotalSeconds - firstTime.Value) / firstTime.Value, 2).ToString(CultureInfo.InvariantCulture), "%|");
+					}
+				}
 			}
 
-			resultWriter.WriteLine();
+			return lines;
 		}
 	}
 }
