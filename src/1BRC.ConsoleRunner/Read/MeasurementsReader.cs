84c6189a7a9fcc46de65b1786343b7bcced70d3f
thoeltig <41591511+thoeltig@users.noreply.github.com>
2025-03-03 02:09:33 +0100

    - moved temperatureContainer into reader file for easier comparison

--- src/1BRC.ConsoleRunner/Read/MeasurementsReader.cs
+++ src/1BRC.ConsoleRunner/Read/MeasurementsReader.cs
@@ -28,4 +28,28 @@ namespace _1BRC.Framework.Console.Read {
 			return dic;
 		}
 	}
+    
+	internal class TemperatureContainer {
+		public double Min { get; private set; }
+
+		public double Average { get; private set; }
+
+		public double Max { get; private set; }
+
+		public TemperatureContainer(double temperature) {
+			Average = temperature;
+			Min = temperature;
+			Max = temperature;
+		}
+
+		public void Update(double temperature) {
+			Average = (Average + temperature) / 2.0;
+
+			if (temperature < Min) {
+				Min = temperature;
+			} else if (temperature > Max) {
+				Max = temperature;
+			}
+		}
+	}
 }
