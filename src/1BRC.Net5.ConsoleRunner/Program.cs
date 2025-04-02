bcb17aca15ca202c5aae3b2786f34c9d229e8078
thoeltig <41591511+thoeltig@users.noreply.github.com>
2025-04-02 22:12:20 +0200

    - implemented read logic in net 5 with spans and only safe code

--- src/1BRC.Net5.ConsoleRunner/Program.cs
+++ src/1BRC.Net5.ConsoleRunner/Program.cs
@@ -1,6 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using _1BRC.Net5.ConsoleRunner.Generate;
+using _1BRC.Net5.ConsoleRunner.Read;
 
 namespace _1BRC.Net5.ConsoleRunner {
 	internal class Program {
@@ -9,19 +11,50 @@ namespace _1BRC.Net5.ConsoleRunner {
 		#else
 		private const int RowCount = 1000000000;
 		#endif
-		private const string FileName = "measurements.txt";
+		private const int MaxReadSpeed = 100 * 1000 * 1000;
+		private const string MeasurementsFileName = "measurements.txt";
+		private const string ResultFileName = "result.txt";
 
 		private static void Main(string[] args) {
-			System.Console.WriteLine($"Generating {RowCount:N0} rows");
-			System.Console.WriteLine();
-			File.Delete(FileName);
-
 			var sw = new Stopwatch();
-			sw.Restart();
-			MeasurementsGenerator.GenerateFile(FileName, RowCount);
-			sw.Stop();
+			if (File.Exists(MeasurementsFileName) == false) {
+				System.Console.WriteLine($"Generating {RowCount:N0} rows");
 
-			System.Console.WriteLine($"Time: {sw.Elapsed:mm':'ss':'fff}");
+				sw.Start();
+				MeasurementsGenerator.GenerateFile(MeasurementsFileName, RowCount);
+				sw.Stop();
+
+				System.Console.WriteLine($"Time: {sw.Elapsed:mm':'ss':'fff}");
+			}
+
+			IReadOnlyCollection<TemperatureContainer> result = null;
+			for (var i = 0; i < 4; i++) {
+				System.Console.WriteLine();
+				System.Console.WriteLine("Reading file");
+
+				sw.Reset();
+				sw.Start();
+				result = MeasurementsReader.ReadFile(MeasurementsFileName);
+				sw.Stop();
+                
+				System.Console.WriteLine($"Time: {sw.Elapsed:mm':'ss':'fff}");
+			}
+
+			if (result != null) {
+				using (var writer = new StreamWriter(ResultFileName, false)) {
+					const string separator = " | ";
+
+					writer.WriteLine("name | min | average | max");
+					writer.WriteLine();
+
+					foreach (var container in result) {
+						writer.WriteLine(string.Concat(container.Name, separator, container.Min, separator, container.Average, separator, container.Max));
+					}
+				}
+
+				System.Console.WriteLine($"Result: {ResultFileName}");
+			}
+
 			System.Console.ReadKey();
 		}
 	}
---
+++ src/1BRC.Net5.ConsoleRunner/Read/MeasurementsReader.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1BRC.Net5.ConsoleRunner.Read {
+	internal static class MeasurementsReader {
+		public static IReadOnlyCollection<TemperatureContainer> ReadFile(string filePath) {
+			#if DEBUG
+			int maxParallel = 1;
+			#else
+			var maxParallel = Environment.ProcessorCount;
+			#endif
+
+			const int blockSize = 131072;
+			const int lineSize = 106;
+			var tasks = new Task[maxParallel];
+			var blockSizes = new byte[blockSize * maxParallel];
+			var lines = new byte[lineSize * maxParallel];
+
+			var offset = 0;
+			var isRunning = true;
+			var dic = new Dictionary<uint, TemperatureContainer>();
+			using (var reader = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read, FileShare.None, 8192, FileOptions.SequentialScan | FileOptions.WriteThrough)) {
+				do {
+					for (var i = 0; i < maxParallel; i++) {
+						var ptr = blockSizes.AsSpan(i * blockSize + offset, blockSize - offset);
+						int readBytes;
+						if ((readBytes = reader.Read(ptr)) != 0) {
+							isRunning = readBytes == blockSize - offset;
+							tasks[i] = ReadLinesAsync(isRunning, dic, blockSizes.AsSpan(i * blockSize, readBytes + offset), readBytes + offset, lines.AsSpan(i * lineSize, lineSize));
+							offset = 0;
+						} else {
+							tasks[i] = Task.FromResult(0);
+						}
+					}
+
+					Task.WaitAll(tasks);
+
+					offset = tasks[0] is Task<int> t ? t.Result : 0;
+					var firstBlock = blockSizes.AsSpan(0, blockSize);
+					for (var i = 1; i < maxParallel; i++) {
+						if (tasks[i] is not Task<int> resultTask || resultTask.Result == 0) {
+							continue;
+						}
+
+						var length = resultTask.Result;
+						blockSizes.AsSpan(i * blockSize, length).CopyTo(firstBlock.Slice(offset, length));
+						offset += length;
+					}
+				} while (isRunning);
+			}
+
+			Array.Clear(blockSizes, 0, blockSizes.Length);
+			Array.Clear(lines, 0, lines.Length);
+			Array.Clear(tasks, 0, tasks.Length);
+			return dic.Values;
+		}
+
+		private static Task<int> ReadLinesAsync(bool ignoreStartAndEndPart, Dictionary<uint, TemperatureContainer> dic, Span<byte> blockPtr, int readBytes, Span<byte> linePtr) {
+			const byte lineSeparator = 59; // ;
+			const byte signedSymbol = 45; // -
+			const byte temperatureSeparator = 46; // .
+			const byte newLine = 10; // \n
+
+			var idx = 0;
+			var nameLength = 0;
+			var flag = IntParseFlag.None;
+			var temperature = 0;
+			int? firstLineEndIdx = null;
+			int? lastLineEndIdx = null;
+
+			for (var bufferIdx = 0; bufferIdx < readBytes; bufferIdx++) {
+				var value = blockPtr[bufferIdx];
+
+				switch (value) {
+					case lineSeparator: {
+						nameLength = idx;
+						break;
+					}
+					case temperatureSeparator: {
+						flag |= IntParseFlag.HasDot;
+						break;
+					}
+					case signedSymbol: {
+						flag |= IntParseFlag.Signed;
+						break;
+					}
+					case newLine: {
+						if (firstLineEndIdx.HasValue == false && ignoreStartAndEndPart) {
+							firstLineEndIdx = bufferIdx;
+							idx = 0;
+							flag = IntParseFlag.None;
+							continue;
+						}
+
+						lastLineEndIdx = bufferIdx;
+
+						// get key with hash logic
+						var key = 2166136261;
+						unchecked {
+							for (var i = 0; i < nameLength; i++) {
+								key = (key * 16777619) ^ linePtr[i];
+							}
+						}
+
+						// parse int -> double or single digit with or without fraction
+						switch (flag) {
+							case IntParseFlag.Signed | IntParseFlag.HasDot: {
+								temperature = -((idx - nameLength) switch {
+									3 => (linePtr[nameLength] - 48) * 100 + (linePtr[nameLength + 1] - 48) * 10 + linePtr[nameLength + 2] - 48,
+									_ => (linePtr[nameLength] - 48) * 10 + (linePtr[nameLength + 1] - 48)
+								});
+								break;
+							}
+							case IntParseFlag.HasDot: {
+								temperature = (idx - nameLength) switch {
+									3 => (linePtr[nameLength] - 48) * 100 + (linePtr[nameLength + 1] - 48) * 10 + linePtr[nameLength + 2] - 48,
+									_ => (linePtr[nameLength] - 48) * 10 + (linePtr[nameLength + 1] - 48)
+								};
+								break;
+							}
+							case IntParseFlag.Signed: {
+								temperature = -10 * (idx - nameLength) switch {
+									2 => (linePtr[nameLength] - 48) * 10 + linePtr[nameLength + 1] - 48,
+									_ => linePtr[nameLength] - 48
+								};
+								break;
+							}
+							case IntParseFlag.None: {
+								temperature = 10 * (idx - nameLength) switch {
+									2 => (linePtr[nameLength] - 48) * 10 + linePtr[nameLength + 1] - 48,
+									_ => linePtr[nameLength] - 48
+								};
+								break;
+							}
+						}
+
+						if (dic.TryGetValue(key, out var container)) {
+							container.Update(temperature);
+						} else {
+							dic.Add(key, new TemperatureContainer(Encoding.UTF8.GetString(linePtr.Slice(0, nameLength)), temperature));
+						}
+
+						idx = 0;
+						flag = IntParseFlag.None;
+						break;
+					}
+					default: {
+						linePtr[idx++] = value;
+						break;
+					}
+				}
+			}
+
+			var firstIdx = firstLineEndIdx ?? 0;
+			var lastIdx = lastLineEndIdx ?? 0;
+			if (ignoreStartAndEndPart == false || (firstIdx == 0 && lastIdx == 0)) {
+				return Task.FromResult(0);
+			}
+
+			var lastCount = 0;
+			if (lastIdx != 0) {
+				lastCount = readBytes - lastIdx;
+			}
+
+			if (lastCount != 0) {
+				blockPtr.Slice(lastIdx, lastCount).CopyTo(blockPtr.Slice(firstIdx, lastCount));
+			}
+
+			return Task.FromResult(firstIdx + lastCount);
+		}
+
+		[Flags]
+		private enum
+			IntParseFlag : byte {
+			None = 0,
+			Signed = 1,
+			HasDot = 2
+		}
+	}
+
+	internal class TemperatureContainer {
+		private const double Divider = 10.0;
+
+		#region
+
+		private int _min;
+		private int _sum;
+		private int _max;
+		private int _count;
+
+		#endregion
+
+		public TemperatureContainer(string name, int temperature) {
+			Name = name;
+			_sum = temperature;
+			_min = temperature;
+			_max = temperature;
+			_count = 1;
+		}
+
+		public string Name { get; }
+
+		public double Min => _min / Divider;
+
+		public double Average => _sum / Divider / _count;
+
+		public double Max => _max / Divider;
+
+		public void Update(int temperature) {
+			_sum += temperature;
+			_count++;
+
+			if (temperature < _min) {
+				_min = temperature;
+			} else if (temperature > _max) {
+				_max = temperature;
+			}
+		}
+	}
+}
